c195df61c362c0d98e97edcf8ae6bec6ddee391d
Aleksey Peredelskiy <info@web-n-roll.ru>
2013-01-13 12:15:28 +0400

    xcache

--- core/cache/xcachestore.inc.php
+++ core/cache/xcachestore.inc.php
@@ -12,12 +12,12 @@
  *
  * @package cache
  */
-class CSX_Cache_ApcStore extends CSX_Cache_Store
+class CSX_Cache_XCacheStore extends CSX_Cache_Store
 {
     function __construct($params = array())
     {
-        if (!function_exists('apc_store')) {
-            throw new CSX_Exception('Looks like no APC extension is installed');
+        if (!function_exists('xcache_get')) {
+            throw new CSX_Exception('Looks like no XCache extension is installed');
         }
 
         parent::__construct($params);
@@ -25,21 +25,21 @@ class CSX_Cache_ApcStore extends CSX_Cache_Store
 
     protected function store($key, $data, $compression = 0, $ttl = null)
     {
-        apc_store($key, $this->compress(serialize($data), $compression), $ttl);
+        xcache_set($key, $this->compress(serialize($data), $compression), $ttl);
     }
 
     protected function fetch($key)
     {
-        return unserialize($this->uncompress(apc_fetch($key)));
+        return unserialize($this->uncompress(xcache_get($key)));
     }
 
     protected function remove($key)
     {
-        apc_delete($key);
+        xcache_unset($key);
     }
 
     function flush()
     {
-        apc_clear_cache($this->getPrefix());
+        xcache_unset_by_prefix($this->getPrefix());
     }
 }
