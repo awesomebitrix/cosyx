6bb4d5d7bb5dc8668bdcf4633212b16bcfa2cc59
Aleksey Peredelskiy <info@web-n-roll.ru>
2013-07-03 18:05:10 +0400

    + action

---
+++ lib/action.inc.php
@@ -0,0 +1,8 @@
+<?php
+class CSX_Action {
+	public static function handle($action, $fn) {
+		if (CSX_Server::getRequest()->get('action')==$action) {
+			call_user_func_array($fn, array($action));
+		}
+	}
+}
