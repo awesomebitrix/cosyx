9eeece32b617efd7bcd1740115761c433ed445d1
Aleksey Peredelskiy <info@web-n-roll.ru>
2013-10-13 20:49:29 +0400

    f

--- lib/iblock.inc.php
+++ lib/iblock.inc.php
@@ -13,285 +13,323 @@
  */
 class CSX_IBlock extends CSX_Singleton
 {
-	protected function __construct($args = array())
-	{
-		CModule::IncludeModule('iblock');
-	}
+    protected function __construct($args = array())
+    {
+        CModule::IncludeModule('iblock');
+    }
 
-	/**
-	 * @return CSX_IBlock
-	 */
-	public static function getInstance()
-	{
-		return self::_getInstance(__CLASS__);
-	}
+    /**
+     * @return CSX_IBlock
+     */
+    public static function getInstance()
+    {
+        return self::_getInstance(__CLASS__);
+    }
 
-	public function getById($id)
-	{
-		$rs = CIBlockElement::GetByID($id);
-		if ($ar = $rs->GetNext()) {
-			return $ar;
-		}
-		else {
-			return null;
-		}
-	}
+    public function getById($id)
+    {
+        $rs = CIBlockElement::GetByID($id);
+        if ($ar = $rs->GetNext()) {
+            return $ar;
+        } else {
+            return null;
+        }
+    }
 
-	public function getByIdElement($id)
-	{
-		$rs = CIBlockElement::GetByID($id);
-		if ($ar = $rs->GetNextElement()) {
-			return $ar;
-		}
-		else {
-			return null;
-		}
-	}
+    public function getByIdElement($id)
+    {
+        $rs = CIBlockElement::GetByID($id);
+        if ($ar = $rs->GetNextElement()) {
+            return $ar;
+        } else {
+            return null;
+        }
+    }
 
-	public function getByIdFull($id, $options = array())
-	{
-		$rs = CIBlockElement::GetByID($id);
-		if ($el = $rs->GetNextElement()) {
-			$ar = $el->GetFields();
-			$ar['PROPERTIES'] = $el->GetProperties();
+    public function getByIdFull($id, $options = array())
+    {
+        $rs = CIBlockElement::GetByID($id);
+        if ($el = $rs->GetNextElement()) {
+            $ar = $el->GetFields();
+            $ar['PROPERTIES'] = $el->GetProperties();
 
-			if (isset($options['resolve_links'])) {
-				foreach ($ar['PROPERTIES'] as &$arValue) {
-					if ($arValue['PROPERTY_TYPE']=='E') {
-						$arValue['VALUE'] = $this->getByIdFull($arValue['VALUE']);
-					}
-				}
-			}
+            if (isset($options['resolve_links'])) {
+                foreach ($ar['PROPERTIES'] as &$arValue) {
+                    if ($arValue['PROPERTY_TYPE'] == 'E') {
+                        $arValue['VALUE'] = $this->getByIdFull($arValue['VALUE']);
+                    }
+                }
+            }
 
-			return $ar;
-		}
-		else {
-			return null;
-		}
-	}
+            return $ar;
+        } else {
+            return null;
+        }
+    }
 
-	public function getByIdCached($id, $expire = 600)
-	{
-		$cache = CSX_Cache::getStore();
-		$key = 'iblock_element_' . $id;
+    public function getByIdCached($id, $expire = 600)
+    {
+        $cache = CSX_Cache::getStore();
+        $key = 'iblock_element_' . $id;
 
-		if (!is_array($ar = $cache->get($key))) {
-			$ar = $this->getById($id);
-			$cache->set($key, $ar, 0, $expire);
-		}
+        if (!is_array($ar = $cache->get($key))) {
+            $ar = $this->getById($id);
+            $cache->set($key, $ar, 0, $expire);
+        }
 
-		return $ar;
-	}
+        return $ar;
+    }
 
-	public function getByIdElementCached($id, $expire = 600)
-	{
-		$cache = CSX_Cache::getStore();
-		$key = 'iblock_eelement_' . $id;
+    public function getByIdElementCached($id, $expire = 600)
+    {
+        $cache = CSX_Cache::getStore();
+        $key = 'iblock_eelement_' . $id;
 
-		if (!is_array($ar = $cache->get($key))) {
-			$ar = $this->getByIdElement($id);
-			$cache->set($key, $ar, 0, $expire);
-		}
+        if (!is_array($ar = $cache->get($key))) {
+            $ar = $this->getByIdElement($id);
+            $cache->set($key, $ar, 0, $expire);
+        }
 
-		return $ar;
-	}
+        return $ar;
+    }
 
-	public function getList($arOrder = array("SORT" => "ASC"),
-							$arFilter = array(),
-							$arGroupBy = false,
-							$arNavStartParams = false,
-							$arSelectFields = array(),
-							$arOptions = array()
-	)
-	{
-		$rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-		if (isset($arOptions['executed'])) {
-			$arOptions['executed']($rs);
-		}
+    public function getList(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array(),
+        $arOptions = array()
+    ) {
+        $obj = $this->getListEx($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields, $arOptions);
+        return $obj['rows'];
+    }
 
-		$rows = array();
-		while ($ar = $rs->GetNext()) {
-			$rows[] = $ar;
-		}
+    public function getListEx(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array(),
+        $arOptions = array()
+    ) {
+        $rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+        if (isset($arOptions['executed'])) {
+            $arOptions['executed']($rs);
+        }
 
-		return $rows;
-	}
+        $rows = array();
+        while ($ar = $rs->GetNext()) {
+            $rows[] = $ar;
+        }
 
-	public function getListElement($arOrder = array("SORT" => "ASC"),
-							$arFilter = array(),
-							$arGroupBy = false,
-							$arNavStartParams = false,
-							$arSelectFields = array())
-	{
-		$rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-		$rows = array();
-		while ($ar = $rs->GetNextElement()) {
-			$rows[] = $ar;
-		}
+        return array(
+            'rows' => $rows,
+            'rowcount' => $rs->NavRecordCount,
+            'pagecount' => $rs->NavPageCount,
+            'page' => $rs->NavPageNomer,
+            'pagesize' => $rs->NavPageSize,
+        );
+    }
 
-		return $rows;
-	}
+    public function getListElement(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array()
+    ) {
+        $rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+        $rows = array();
+        while ($ar = $rs->GetNextElement()) {
+            $rows[] = $ar;
+        }
 
-	public function getListCached($arOrder = array("SORT" => "ASC"),
-								  $arFilter = array(),
-								  $arGroupBy = false,
-								  $arNavStartParams = false,
-								  $arSelectFields = array(),
-								  $expire = 600)
-	{
-		$cache = CSX_Cache::getStore();
-		$key = 'iblock_list_' . $this->getKey($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+        return $rows;
+    }
 
-		if (!is_array($rows = $cache->get($key))) {
-			$rows = $this->getList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-			$cache->set($key, $rows, 0, $expire);
-		}
+    public function getListCached(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array(),
+        $expire = 600
+    ) {
+        $cache = CSX_Cache::getStore();
+        $key = 'iblock_list_' . $this->getKey($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
 
-		return $rows;
-	}
+        if (!is_array($rows = $cache->get($key))) {
+            $rows = $this->getList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+            $cache->set($key, $rows, 0, $expire);
+        }
 
-	public function getListHash($arOrder = array("SORT" => "ASC"),
-							$arFilter = array(),
-							$arGroupBy = false,
-							$arNavStartParams = false,
-							$arSelectFields = array())
-	{
-		$rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-		$rows = array();
-		while ($ar = $rs->GetNext()) {
-			$rows[$ar['ID']] = $ar;
-		}
+        return $rows;
+    }
 
-		return $rows;
-	}
+    public function getListHash(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array()
+    ) {
+        $rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+        $rows = array();
+        while ($ar = $rs->GetNext()) {
+            $rows[$ar['ID']] = $ar;
+        }
 
-	public function getListHashCached($arOrder = array("SORT" => "ASC"),
-								  $arFilter = array(),
-								  $arGroupBy = false,
-								  $arNavStartParams = false,
-								  $arSelectFields = array(),
-								  $expire = 600)
-	{
-		$cache = CSX_Cache::getStore();
-		$key = 'iblock_list_' . $this->getKey($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+        return $rows;
+    }
 
-		if (!is_array($rows = $cache->get($key))) {
-			$rows = $this->getListHash($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-			$cache->set($key, $rows, 0, $expire);
-		}
+    public function getListHashCached(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array(),
+        $expire = 600
+    ) {
+        $cache = CSX_Cache::getStore();
+        $key = 'iblock_list_' . $this->getKey($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
 
-		return $rows;
-	}
+        if (!is_array($rows = $cache->get($key))) {
+            $rows = $this->getListHash($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+            $cache->set($key, $rows, 0, $expire);
+        }
 
-	public function getSingle($arOrder = array("SORT" => "ASC"),
-							  $arFilter = array(),
-							  $arGroupBy = false,
-							  $arNavStartParams = false,
-							  $arSelectFields = array())
-	{
-		$rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-		if ($ar = $rs->GetNext()) {
-			return $ar;
-		}
-		else {
-			return null;
-		}
-	}
+        return $rows;
+    }
 
-	public function getCount($arFilter = array())
-	{
-		return CIBlockElement::GetList(false, $arFilter, array(), false, false);
-	}
+    public function getSingle(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array()
+    ) {
+        $rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+        if ($ar = $rs->GetNext()) {
+            return $ar;
+        } else {
+            return null;
+        }
+    }
 
-	public function getSingleCached($arOrder = array("SORT" => "ASC"),
-								  $arFilter = array(),
-								  $arGroupBy = false,
-								  $arNavStartParams = false,
-								  $arSelectFields = array(),
-								  $expire = 600)
-	{
-		$cache = CSX_Cache::getStore();
-		$key = 'iblock_list_single_' . $this->getKey($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+    public function getCount($arFilter = array())
+    {
+        return CIBlockElement::GetList(false, $arFilter, array(), false, false);
+    }
 
-		if (!is_array($ar = $cache->get($key))) {
-			$ar = $this->getSingle($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-			$cache->set($key, $ar, 0, $expire);
-		}
+    public function getSingleCached(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array(),
+        $expire = 600
+    ) {
+        $cache = CSX_Cache::getStore();
+        $key = 'iblock_list_single_' . $this->getKey(
+            $arOrder,
+            $arFilter,
+            $arGroupBy,
+            $arNavStartParams,
+            $arSelectFields
+        );
 
-		return $ar;
-	}
+        if (!is_array($ar = $cache->get($key))) {
+            $ar = $this->getSingle($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+            $cache->set($key, $ar, 0, $expire);
+        }
 
-	public function getSingleElement($arOrder = array("SORT" => "ASC"),
-		$arFilter = array(),
-		$arGroupBy = false,
-		$arNavStartParams = false,
-		$arSelectFields = array())
-	{
-		$rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-		if ($ar = $rs->GetNextElement()) {
-			return $ar;
-		}
-		else {
-			return null;
-		}
-	}
+        return $ar;
+    }
 
-	public function getSingleElementCached($arOrder = array("SORT" => "ASC"),
-		$arFilter = array(),
-		$arGroupBy = false,
-		$arNavStartParams = false,
-		$arSelectFields = array(),
-		$expire = 600)
-	{
-		$cache = CSX_Cache::getStore();
-		$key = 'iblock_list_esingle_' . $this->getKey($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+    public function getSingleElement(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array()
+    ) {
+        $rs = CIBlockElement::GetList($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+        if ($ar = $rs->GetNextElement()) {
+            return $ar;
+        } else {
+            return null;
+        }
+    }
 
-		if (!is_array($ar = $cache->get($key))) {
-			$ar = $this->getSingleElement($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
-			$cache->set($key, $ar, 0, $expire);
-		}
+    public function getSingleElementCached(
+        $arOrder = array("SORT" => "ASC"),
+        $arFilter = array(),
+        $arGroupBy = false,
+        $arNavStartParams = false,
+        $arSelectFields = array(),
+        $expire = 600
+    ) {
+        $cache = CSX_Cache::getStore();
+        $key = 'iblock_list_esingle_' . $this->getKey(
+            $arOrder,
+            $arFilter,
+            $arGroupBy,
+            $arNavStartParams,
+            $arSelectFields
+        );
 
-		return $ar;
-	}
+        if (!is_array($ar = $cache->get($key))) {
+            $ar = $this->getSingleElement($arOrder, $arFilter, $arGroupBy, $arNavStartParams, $arSelectFields);
+            $cache->set($key, $ar, 0, $expire);
+        }
 
-	protected function getKey()
-	{
-		$args = func_get_args();
-		return md5(serialize($args));
-	}
+        return $ar;
+    }
 
-	public function setUrlTemplates(&$rs, $arSubst = array()) {
-		foreach ($rs as &$arItem) {
-			$this->setUrlTemplatesItem($arItem, $arSubst);
-		}
-	}
+    protected function getKey()
+    {
+        $args = func_get_args();
 
-	public function setUrlTemplatesItem(&$arItem, $arSubst = array()) {
-		foreach ($arSubst as $key => $val) {
-			$arItem['LIST_PAGE_URL'] = str_replace("#{$key}#", $val, $arItem['LIST_PAGE_URL']);
-			$arItem['DETAIL_PAGE_URL'] = str_replace("#{$key}#", $val, $arItem['DETAIL_PAGE_URL']);
-			$arItem['SECTION_PAGE_URL'] = str_replace("#{$key}#", $val, $arItem['SECTION_PAGE_URL']);
-		}
+        return md5(serialize($args));
+    }
 
-		$this->setUrlTemplatesItemEx($arItem);
-	}
+    public function setUrlTemplates(&$rs, $arSubst = array())
+    {
+        foreach ($rs as &$arItem) {
+            $this->setUrlTemplatesItem($arItem, $arSubst);
+        }
+    }
 
-	protected function setUrlTemplatesItemEx(&$arItem, $stack = array(), &$arSubItem = null) {
-		$prefix = !empty($stack) ? implode('_', $stack) . '_' : '';
-		if ($arSubItem===null) $arSubItem = &$arItem;
+    public function setUrlTemplatesItem(&$arItem, $arSubst = array())
+    {
+        foreach ($arSubst as $key => $val) {
+            $arItem['LIST_PAGE_URL'] = str_replace("#{$key}#", $val, $arItem['LIST_PAGE_URL']);
+            $arItem['DETAIL_PAGE_URL'] = str_replace("#{$key}#", $val, $arItem['DETAIL_PAGE_URL']);
+            $arItem['SECTION_PAGE_URL'] = str_replace("#{$key}#", $val, $arItem['SECTION_PAGE_URL']);
+        }
 
-		foreach ($arSubItem as $key => $val) {
-			if (is_array($val)) {
-				$cstack = $stack;
-				$cstack[] = $key;
-				$this->setUrlTemplatesItemEx($arItem, $cstack, $val);
-			}
-			else {
-				$arItem['LIST_PAGE_URL'] = str_replace("#{$prefix}{$key}#", $val, $arItem['LIST_PAGE_URL']);
-				$arItem['DETAIL_PAGE_URL'] = str_replace("#{$prefix}{$key}#", $val, $arItem['DETAIL_PAGE_URL']);
-				$arItem['SECTION_PAGE_URL'] = str_replace("#{$prefix}{$key}#", $val, $arItem['SECTION_PAGE_URL']);
-			}
-		}
+        $this->setUrlTemplatesItemEx($arItem);
+    }
 
-	}
+    protected function setUrlTemplatesItemEx(&$arItem, $stack = array(), &$arSubItem = null)
+    {
+        $prefix = !empty($stack) ? implode('_', $stack) . '_' : '';
+        if ($arSubItem === null) {
+            $arSubItem = & $arItem;
+        }
+
+        foreach ($arSubItem as $key => $val) {
+            if (is_array($val)) {
+                $cstack = $stack;
+                $cstack[] = $key;
+                $this->setUrlTemplatesItemEx($arItem, $cstack, $val);
+            } else {
+                $arItem['LIST_PAGE_URL'] = str_replace("#{$prefix}{$key}#", $val, $arItem['LIST_PAGE_URL']);
+                $arItem['DETAIL_PAGE_URL'] = str_replace("#{$prefix}{$key}#", $val, $arItem['DETAIL_PAGE_URL']);
+                $arItem['SECTION_PAGE_URL'] = str_replace("#{$prefix}{$key}#", $val, $arItem['SECTION_PAGE_URL']);
+            }
+        }
+
+    }
 }
