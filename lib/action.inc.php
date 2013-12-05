b3f56755d2fa2d80f52b104760fc4a277c2a893b
Aleksey Peredelskiy <info@web-n-roll.ru>
2013-12-05 10:23:28 +0400

    f

--- lib/action.inc.php
+++ lib/action.inc.php
@@ -1,10 +1,11 @@
 <?php
 class CSX_Action
 {
-    public static function handle($action, $fn)
+    public static function handle($action, $fn, $params = array())
     {
         if (CSX_Server::getRequest()->get('action') == $action) {
-            call_user_func_array($fn, array($action));
+            array_unshift($params, $action);
+            call_user_func_array($fn, $params);
         }
     }
 }
